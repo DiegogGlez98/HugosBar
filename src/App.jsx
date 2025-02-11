0defea3cc886d1dede711f0f7ce583682614b5ce
diegoglez <diegoglezh98@gmail.com>
2025-02-11 13:30:41 -0600

    navBar e imagen principal

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <main>
+        <div>
+          <Home/>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
+

97f28f9f13f6b654c178177746a8f031f1c31724
diegoglez <diegoglezh98@gmail.com>
2025-04-08 13:51:59 -0600

    deploy3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/HugosBar",
+  base: "https://DiegogGlez98.github.io/HugosBar",
   plugins: [react()],
 });
